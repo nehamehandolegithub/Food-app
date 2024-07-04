663f5cae3fe27e2ce8b8ab19cb677a2b88dc326d
nehamehandole2001@gmail.com <neha@leocoders.com>
2024-07-04 10:40:52 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "../pages/HomePage/HomePage";
+import Category from "../pages/CategoryPage/CategoryPage";
+import Error from "../pages/ErrorPage/ErrorPage"
+import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
+
+export {Home,Category,Error,MealDetails};
